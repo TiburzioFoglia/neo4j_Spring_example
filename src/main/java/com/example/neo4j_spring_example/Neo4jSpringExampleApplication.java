a29c8c33485e366f7d4c000a0e213e41520cef82
TiburzioFoglia <simone.debernardinis@studenti.unicam.it>
2022-05-23 12:47:32 +0200

    init
    init

---
+++ src/main/java/com/example/neo4j_spring_example/Neo4jSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.neo4j_spring_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jSpringExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/neo4j_spring_example/Neo4jSpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neo4j_spring_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jSpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
